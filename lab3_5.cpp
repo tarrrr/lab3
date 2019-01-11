a00b53f582455f5c51997ec14a2282ee60bd6730
lab314-47@labeng.local <lab314-47@labeng.local>
2019-01-11 17:30:47 +0700

    lab3_5

---
+++ lab3_5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string text1,text2;
+    double N;
+    int count;
+    cout<<"Enter the first text: ";
+    cin>>text1;
+    cout<<"Enter the second text: ";
+    cin>>text2;
+    cout<<"Enter N: ";
+    cin>>N;
+    count = 0;
+    while(count<N){
+        if(count%2==0){
+        cout<<text1<<" ";
+    }else{
+        cout<<text2<<" ";
+        
+    }
+    count++;
+    }
+    return 0;
+}
