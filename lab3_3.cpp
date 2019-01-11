2e7411705fd8a50d550e94bd0a59529cb45aa6f9
lab314-47@labeng.local <lab314-47@labeng.local>
2019-01-11 16:59:31 +0700

    lab3v2

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-    double n = 2,i=0;
-    while(n<=69){
+    double n = 6,i=0;
+    while(n<=19){
         i = i + (1/n);
         n++;
     }
